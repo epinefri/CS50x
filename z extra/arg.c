dd092676b4c731ca5e7b6edc70aa7a1a63ac2180
epinefri <danielasorinap@gmail.com>
2022-08-25 16:08:45 +0300

    deleted some drafts

--- z extra/arg.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char* argv[])
-{
-    printf("argc: %i\n", argc);
-    printf("argv[0]: %s\n", argv[0]);
-    printf("argv[1]: %s\n", argv[1]);
-    printf("argv[2]: %s\n", argv[2]);
-}
